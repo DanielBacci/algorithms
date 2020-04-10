c146a2bbd49fe80234047ae0a4004b5a5262bd31
DanielBacci <danielhdbacci@gmail.com>
2020-04-10 12:08:17 -0300

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -15,3 +15,19 @@ def sherlockAndAnagrams(word):
     return total
 
 sherlockAndAnagrams('cdcd')
+
+def countTriplets(arr, r):
+    from collections import defaultdict
+
+    v2 = defaultdict(int)
+    v3 = defaultdict(int)
+    count = 0
+
+    for k in arr:
+        count += v3[k]
+        v3[k*r] += v2[k]
+        v2[k*r] += 1
+
+    return count
+
+countTriplets([1, 2, 2, 4], 2)
