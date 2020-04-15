e8e97405f2a022805afe14f27daf5c0b6468f00a
DanielBacci <danielhdbacci@gmail.com>
2020-04-15 00:43:40 -0300

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -8,3 +8,16 @@ def insertNodeAtPosition(head, data, position):
     item.next = node
     return head
     
+def sortedInsert(head, data):
+    node = DoublyLinkedListNode(data)
+    if (head == None):
+        return node
+    elif (data < head.data):
+        node.next = head
+        head.prev = node
+        return node
+    else:
+        node = sortedInsert(head.next, data)
+        head.next = node 
+        node.prev = head
+        return head
