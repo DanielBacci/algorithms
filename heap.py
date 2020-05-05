6cc47013a254ba0758eb8bd19f93008bede8377a
DanielBacci <danielhdbacci@gmail.com>
2020-05-05 00:51:28 -0300

    Create heap.py

---
+++ heap.py
@@ -0,0 +1,13 @@
+import heapq
+
+items = []
+
+heapq.heappush(items, 5)
+heapq.heappush(items, 10)
+heapq.heappush(items, 8)
+heapq.heappush(items, 4)
+heapq.heappush(items, 3)
+
+heapq.heappop(items)
+heapq.heappop(items)
+heapq.heappop(items)
