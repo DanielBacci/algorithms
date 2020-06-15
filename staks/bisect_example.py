0019684ecd75f5acf439a0eb797b6cf6fc3a8053
DanielBacci <danielhdbacci@gmail.com>
2020-06-15 11:03:08 -0300

    Update bisect_example.py

--- staks/bisect_example.py
+++ staks/bisect_example.py
@@ -1,19 +1,23 @@
 from bisect import bisect_left, insort_left
 
 
-def median(values, d):
-    if d % 2 == 1:
-        return values[d // 2]
-    return (values[d // 2] + values[d // 2 - 1]) / 2
+def median(values):
+    midst = len(values) // 2
+
+    if len(values) % 2 == 0:
+        return (values[midst] + values[midst - 1]) / 2
+    else:
+        return values[midst]
 
 
 def activityNotifications(expenditure, d):
     notifications = 0
     last_d = sorted(expenditure[:d])
     for index in range(d, len(expenditure)):
-        if expenditure[index] >= 2 * median(last_d, d):
+        if expenditure[index] >= 2 * median(last_d):
             notifications += 1
 
+        import ipdb; ipdb.set_trace()
         del last_d[bisect_left(last_d, expenditure[index - d])]
         insort_left(last_d, expenditure[index])
     return notifications
