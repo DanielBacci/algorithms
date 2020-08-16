1cefcc42c1b2a7f5eba3ce91219f1358acfd6a03
DanielBacci <danielhdbacci@gmail.com>
2020-08-16 15:01:50 -0300

    Update lcs.py

--- algorithms/lcs.py
+++ algorithms/lcs.py
@@ -3,17 +3,17 @@ def commonChild(s1, s2):
     n = len(s1)
     preview_row = [0] * (n + 1)
     current_row = [0] * (n + 1)
-    for index in range(1, n + 1):
-        for index_s2 in range(1, n + 1):
-            if s1[index - 1] == s2[index_s2 - 1]:
-                current_row[index_s2] = preview_row[index_s2 - 1] + 1
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                current_row[j] = preview_row[j - 1] + 1
             else:
-                if current_row[index_s2 - 1] >= preview_row[index_s2]:
-                    current_row[index_s2] = current_row[index_s2 - 1]
+                if current_row[j - 1] >= preview_row[j]:
+                    current_row[j] = current_row[j - 1]
                 else:
-                    current_row[index_s2] - preview_row[index_s2]
-
+                    current_row[j] = preview_row[j]
         preview_row = current_row
         current_row = [0] * (n + 1)
-
     return preview_row[n]
+
+commonChild("ABCD", "ABDC")
