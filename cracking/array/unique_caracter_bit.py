1007667261255aca18118f7fa2fd0d2d075c36ce
DanielBacci <danielhdbacci@gmail.com>
2022-06-20 15:24:06 -0300

    Update unique_caracter_bit.py

--- cracking/array/unique_caracter_bit.py
+++ cracking/array/unique_caracter_bit.py
@@ -16,8 +16,18 @@ def unique_characters(string):
  
         if ((bit_index) > -1): # Less than -1 is invalid argument
             if ((checker & ((1 << bit_index))) > 0):
-                ### It will make a AND (&). Ex. 0001 & 0010 = 0000
+                ### It will make AND (&). Ex. 0001 & 0010 = 0000
                 ### That's why I need to compare > 0, because of 0010 & 0010 => 0010 => 2.
+                ### 2 << 5 # shift to left by 5 bits
+                ### 2 >> 5 # shift to right by 5 bits
+                
+                ### IMPORTANT!
+                ### ord(string[index]) - ord('a') => 0
+                ### 0 => ((1 << bit_index)) => 1
+                ### 1 => binary => 0001
+                ### 0000 & 0001 => 0000
+                ### 0000 => 0
+         
                 return False
 
             checker = checker | (1 << bit_index)
