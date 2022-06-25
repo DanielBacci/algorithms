86216c8330d74d9159132b4841b155c2221d43f1
DanielBacci <danielhdbacci@gmail.com>
2022-06-25 18:43:59 -0300

    Update pallindrome_permutation.py

--- cracking/array/pallindrome_permutation.py
+++ cracking/array/pallindrome_permutation.py
@@ -1,11 +1,25 @@
-    def canPermutePalindrome(self, s: str) -> bool:
-        from collections import defaultdict
-        hashmap = defaultdict(int)
-        for char in s:
-            hashmap[char] += 1
+def canPermutePalindrome(self, s: str) -> bool:
+    from collections import defaultdict
+    hashmap = defaultdict(int)
+    for char in s:
+        hashmap[char] += 1
 
-        total = 0
-        for k, v in hashmap.items():
-            total += v % 2
+    total = 0
+    for k, v in hashmap.items():
+        total += v % 2
 
-        return total <= 1
+    return total <= 1
+
+def is_permutation_palindrome(string):
+    bit = 0
+    import ipdb; ipdb.set_trace()
+    for char in string:
+        char_ord = ord(char)
+
+        mask = 1 << char_ord
+        if (bit & mask) == 0:
+            bit |= mask
+        else:
+            bit &= ~mask
+
+    return (bit & (bit - 1)) == 0
