a7e2932374dcd663cdbb48e09ff8222f7e55bbf9
DanielBacci <danielhdbacci@gmail.com>
2025-09-01 15:44:52 -0400

    Update 238.py

--- array/238.py
+++ array/238.py
@@ -1,16 +1,16 @@
-def product_except_self(numbers):
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre = 1
+        cache_result = []
+        for index, number in enumerate(nums):
+            cache_result.append(pre)
+            pre *= number
 
-    acc_pre = 1
-    cache_result = []
-    for index, number in enumerate(numbers):
-        cache_result.append(acc_pre)
-        acc_pre *= number
+        pos = 1
+        n = len(nums)
+        for i, number in enumerate(reversed(nums)):
+            original_index = n - 1 - i
+            cache_result[original_index] *= pos
+            pos *= nums[original_index]
 
-    acc_pos = 1
-    n = len(numbers)
-    for i, number in enumerate(reversed(numbers)):
-        original_index = n - 1 - i
-        cache_result[original_index] *= acc_pos
-        acc_pos *= numbers[original_index]
-
-    return cache_result
+        return cache_result
